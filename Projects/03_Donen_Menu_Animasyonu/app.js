675ee0fc36d000bac18c104313cebecf8207681b
Ozan Tekin <ardilozantekin@gmail.com>
2022-04-07 12:14:18 +0300

    Update app.js

--- Projects/03_Donen_Menu_Animasyonu/app.js
+++ Projects/03_Donen_Menu_Animasyonu/app.js
@@ -1,11 +1,11 @@
-const open = document.getElementById('open');
-const close  = document.getElementById('close');
-const container = document.querySelector('.container');
+const open = document.getElementById('open')
+const close = document.getElementById('close')
+const container = document.querySelector('.container')
 
 open.addEventListener('click', () => {
-    container.classList.add('show-nav')
+  container.classList.add('show-nav')
 })
 
-close.addEventListener("click", () => {
-    container.classList.remove('show-nav')
+close.addEventListener('click', () => {
+  container.classList.remove('show-nav')
 })
