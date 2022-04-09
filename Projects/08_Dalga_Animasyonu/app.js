64ad7744f84e64b6c357a9860ba914bc84c1627e
Ozan Tekin <ardilozantekin@gmail.com>
2022-04-09 22:39:39 +0300

    8.proje tamamlandı

---
+++ Projects/08_Dalga_Animasyonu/app.js
@@ -0,0 +1,11 @@
+const labels = document.querySelectorAll('.form-control label')
+
+labels.forEach((label) => {
+  label.innerHTML = label.innerText
+    .split('')
+    .map(
+      (letter, index) =>
+        `<span style="transition-delay:${index * 50}ms">${letter}</span>`
+    )
+    .join('')
+}).fomr
