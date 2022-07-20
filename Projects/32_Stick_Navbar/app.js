18227929a85421354053351d13b099221075bdf1
Ozan Tekin <ardilozantekin@gmail.com>
2022-07-20 12:03:39 +0300

    added sticky navbar

---
+++ Projects/32_Stick_Navbar/app.js
@@ -0,0 +1,12 @@
+const nav = document.querySelector('.nav')
+
+const add = 150
+
+window.addEventListener('scroll', () => {
+  // console.log(window.scrollY, nav.offsetHeight)
+  if (window.scrollY > nav.offsetHeight + add) {
+    nav.classList.add('move')
+  } else {
+    nav.classList.remove('move')
+  }
+})
